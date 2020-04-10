d7917a3bc728cf32196fdddd9d3621fbe721202d
RahmaneTokyo <62186167+RahmaneTokyo@users.noreply.github.com>
2020-04-10 14:45:29 +0000

    Add files via upload

---
+++ mini/conexion.php
@@ -0,0 +1,51 @@
+<?php
+    $login = "";
+    $pass = "";
+    if (isset($_POST['connexion'])) {
+        if(empty($login) || empty($pass)) {
+            $erreur = "Veuillez remplir ce champ !";
+        }else {
+            $login = $_POST['login'];
+            $pass = $_POST['pass'];
+        }
+        var_dump($login);
+        var_dump($pass);
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="sample.css">
+        <title>Connexion</title>
+    </head>
+    <body>
+        <div id="cadre">
+            <div class="tete">
+                <div class="logo"> <img src="Images/logo.jpeg"> </div>
+                <div class="texte">Le plaisir de jouer</div>
+            </div>
+            <div class="fond">
+                <div class="connexion">
+                    <div class="form">
+                        <div class="text">Login Form</div>
+                    </div>
+                    <form method="post" action="">
+                        <div class="log">
+                            <div class="login"> <input type="text" name="login" autocomplete="off" placeholder="Login"> </div>
+                            <div class="icone"> <img src="Images/icônes/ic-login.png"> </div>
+                        </div>
+                        <div class="log">
+                            <div class="login"> <input type="password" name="pass" placeholder="Password"> </div>
+                            <div class="icone"> <img src="Images/icônes/ic-password.png"> </div>
+                        </div>
+                        <button name="connexion">Connexion</button>
+                        <div class="inscription"> <a href="inscription.php"> S'inscrire pour jouer ? </a> </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ mini/inscription.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="sample.css">
+        <title>Inscription</title>
+    </head>
+    <body>
+        <div id="cadre">
+        <div class="tete">
+            <div class="texte"> Le plaisir de jouer </div>
+        </div>
+        <div class="fond">
+            <div class="fond1">
+                <div class="fond2">
+                    <div class="fond3">
+                        <div class="fond4">
+                            <div class="formulaire">
+                                <div class="inscrit">S'INSCRIRE</div>
+                                <div class="petit"> Pour tester votre niveau de culture générale </div>
+                                <form method="post" action="">
+                                    <div class="label"> Prénom </div>
+                                    <div class="champ"> <input type="text"> </div>
+                                    <div class="label"> Nom </div>
+                                    <div class="champ"> <input type="text"> </div>
+                                    <div class="label"> Login </div>
+                                    <div class="champ"> <input type="text"> </div>
+                                    <div class="label"> Password </div>
+                                    <div class="champs"> <input type="password" placeholder="********"> </div>
+                                    <div class="label"> Confirmer Password </div>
+                                    <div class="champs"> <input type="password" placeholder="********"> </div>
+                                    <div class="texteavatar">Avatar</div>
+                                    <div class="fichier">
+                                        <label for="file" class="label-file">Choisir un fichier</label> 
+                                        <input id="file" class="choisir" type="file" accept="image/*">
+                                    </div>
+                                    <div class="creer"> <button> Créer compte </button> </div>
+                                </form>
+                            </div>
+                            <div class="avatar"></div>
+                            <div class="joueur">Avatar du joueur</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>    
+    </body>
+</html>
