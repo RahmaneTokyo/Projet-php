45b29efe3764e1ca5f401e75a8b99353a83ff2e9
RahmaneTokyo <62186167+RahmaneTokyo@users.noreply.github.com>
2020-04-12 19:58:47 +0000

    Update conexion.php

--- mini/conexion.php
+++ mini/conexion.php
@@ -1,3 +1,50 @@
+<?php
+    session_start();
+    $js = file_get_contents("session.json");
+    $js = json_decode($js,true);
+    $message = "";
+    $error = "";
+    if (isset($_POST['connexion'])) {
+        $login= $_POST['login'];
+        $password= $_POST['password'];
+        if (empty($login) && empty($password)){
+            $message.='Veuillez entrer votre login et votre mot de passe';
+
+        }
+        elseif (empty($login)){
+            $has_error=true;
+            $message.='Veuillez entrer votre login';
+
+        }
+        elseif (empty($password)){
+
+            $message.='Veuillez entrer votre mot de passe';
+
+        }else{
+            foreach($js as $value){
+                if ($login== $value['login'] && $password== $value['password']){
+                    $_SESSION['prenom']= $value['prenom'];
+                    $_SESSION['nom']= $value['nom'];
+                    $_SESSION['avatar']= $value['avatar'];
+                    if ($value['role'] == "admin"){
+                        header("Location: admin.php");
+                    }else{
+                        header("Location: admin.php");
+                    }
+
+                }elseif ($login == $value['login'] && $password != $value['password']){
+                    $error='Mot de passe incorrect';
+                }
+                elseif ($login != $value['login'] && $password == $value['password']){
+                    $error='Login incorrect';
+                }elseif($login != $value['login'] && $password != $value['password']){
+                    $error='Login et Mot de passe incorrecte ';
+                }
+            }
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -18,12 +65,16 @@
                         <div class="text">Login Form</div>
                     </div>
                     <form method="post" action="">
+                        <span> <?= $message ?> </span>
+                        <span style="color: red"><?= $error?></span>
                         <div class="log">
-                            <div class="login"> <input type="text" name="login" autocomplete="off" placeholder="Login"> </div>
+                            <div class="login"> <input type="text" name="login" autocomplete="off" placeholder="Login"
+                            value="<?php if(isset($login)) { echo $login; } ?>"> </div>
                             <div class="icone"> <img src="Images/icônes/ic-login.png"> </div>
                         </div>
                         <div class="log">
-                            <div class="login"> <input type="password" name="pass" placeholder="Password"> </div>
+                            <div class="login"> <input type="password" name="password" placeholder="Password"
+                            value="<?php if(isset($password)) { echo $password; } ?>"> </div>
                             <div class="icone"> <img src="Images/icônes/ic-password.png"> </div>
                         </div>
                         <button name="connexion">Connexion</button>
