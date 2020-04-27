728b4becca29a0705d76236b368113870db4c21f
RahmaneTokyo <62186167+RahmaneTokyo@users.noreply.github.com>
2020-04-27 00:15:22 +0000

    Update inscription.php

--- Quizz/pages/inscription.php
+++ Quizz/pages/inscription.php
@@ -13,12 +13,8 @@
         $imageSize = $_FILES['image']['size'];
 
         validation_donnees($login,$pwd,$confirm_pwd,$prenom,$nom,$profil="joueur",$image,$imagePath,$imageExtension,$imageSize);
-
-        header("location:index.php");
     }
 ?>
-
-
         <div class="fond">
             <div class="fond1">
                 <div class="fond2">
@@ -26,14 +22,15 @@
                         <div class="fond4">
                             <div class="formulaire">
                                 <div class="inscrit">S'INSCRIRE</div>
+                                <span><?php if(isset($message)) { echo $message; } ?></span>
                                 <div class="petit"> Pour tester votre niveau de culture générale </div>
-                                <form method="post" action="" id="form-connexion" enctype="multipart/form-data">
+                                <form class="form" method="post" action="" id="form-connexion" enctype="multipart/form-data">
                                     <div class="label"> Prénom </div>
                                     <div class="eror" id="error-1"></div> 
-                                    <div class="champ"> <input type="text" name="prenom" id="prenom" error="error-1" > </div>
+                                    <div class="champ"> <input type="text" name="prenom" id="prenom" error="error-1" value="<?php if(isset($_POST['submit'])) { echo $prenom; } ?>"> </div>
                                     <div class="label"> Nom </div>
                                     <div class="eror" id="error-2"></div>
-                                    <div class="champ"> <input type="text" name="nom" id="nom" error="error-2" > </div>
+                                    <div class="champ"> <input type="text" name="nom" id="nom" error="error-2" value="<?php if(isset($_POST['submit'])) { echo $nom; } ?>"> </div>
                                     <div class="label"> Login </div>
                                     <div class="eror" id="error-3"></div>
                                     <div class="champ"> <input type="text" name="login" id="login" error="error-3" > </div>
@@ -48,11 +45,13 @@
                                         <label for="file" class="label-file">Choisir un fichier</label> 
                                         <input id="file" class="choisir" type="file" name="image" accept="image/*" onchange="loadFile(event)">
                                     </div>
-                                    <div class="creer"> <button type="submit" name="submit" id="connexion"> Créer compte </button> </div>
+                                    <button type="submit" class="creer" name="submit" id="connexion"> Créer compte </button> </div>
                                 </form>
                             </div>
-                            <img id="output" class="avatarr">
-                            <div class="joueeur">Avatar du joueur</div>
+                            <div class="champavatar">
+                                <img id="output" class="avatarr">
+                                <div class="joueeur">Avatar du joueur</div>
+                            </div>
                         </div>
                     </div>
                 </div>
