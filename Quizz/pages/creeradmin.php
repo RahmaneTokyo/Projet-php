19a2a5da644383979e93ee95d955ba82846669d2
RahmaneTokyo <62186167+RahmaneTokyo@users.noreply.github.com>
2020-04-27 00:13:47 +0000

    Update creeradmin.php

--- Quizz/pages/creeradmin.php
+++ Quizz/pages/creeradmin.php
@@ -20,45 +20,47 @@
     }
 ?>
 <div class="fond">
-    <div class="fond1">
-        <div class="fond2">
-            <div class="fond3">
-                <div class="fond4">
-                    <div class="formulaire">
-                        <div class="inscrit">S'INSCRIRE</div>
-                        <div class="petit"> Pour tester votre niveau de culture générale </div>
-                        <form method="post" action="" class="form" enctype="multipart/form-data" id="form-connexion">
-                            <div class="label"> Prénom </div>
-                            <div class="champ"> <input type="text" name="prenom" error="error-1" id="prenom"> </div>
-                            <div class="errors" id="error-1"></div>
-                            <div class="label"> Nom </div>
-                            <div class="champ"> <input type="text" error="error-2" name="nom" id="nom"> </div>
-                            <div class="errors" id="error-2"></div>
-                            <div class="label"> Login </div>
-                            <div class="champ"> <input type="text" error="error-3" name="login" id="login"> </div>
-                            <div class="errors" id="error-3"></div>
-                            <div class="label"> Password </div>
-                            <div class="champs"> <input type="password" error="error-4" name="password" id="password" placeholder="********"> </div>
-                            <div class="errors" id="error-4"></div>
-                            <div class="label"> Confirmer Password </div>
-                            <div class="champs"> <input type="password" name="confirm" error="error-5" id="confirm" placeholder="********"> </div>
-                            <div class="errors" id="error-5"></div>
-                            <div class="texteavatar">Avatar</div>
-                            <div class="fichier">
-                                <label for="file" class="label-file">Choisir un fichier</label>
-                                <input type="hidden" value="1000000" name="MAX_FILE_SIZE">
-                                <input type="file" id="file" class="choisir" name="image" accept="image/*" onchange="loadFile(event)">
+            <div class="fond1">
+                <div class="fond2">
+                    <div class="fond3">
+                        <div class="fond4">
+                            <div class="formulaire">
+                                <div class="inscrit">S'INSCRIRE</div>
+                                <span><?php if(isset($message)) { echo $message; } ?></span>
+                                <div class="petit"> Pour tester votre niveau de culture générale </div>
+                                <form class="form" method="post" action="" id="form-connexion" enctype="multipart/form-data">
+                                    <div class="label"> Prénom </div>
+                                    <div class="eror" id="error-1"></div> 
+                                    <div class="champ"> <input type="text" name="prenom" id="prenom" error="error-1" value="<?php if(isset($_POST['submit'])) { echo $prenom; } ?>"> </div>
+                                    <div class="label"> Nom </div>
+                                    <div class="eror" id="error-2"></div>
+                                    <div class="champ"> <input type="text" name="nom" id="nom" error="error-2" value="<?php if(isset($_POST['submit'])) { echo $nom; } ?>"> </div>
+                                    <div class="label"> Login </div>
+                                    <div class="eror" id="error-3"></div>
+                                    <div class="champ"> <input type="text" name="login" id="login" error="error-3" > </div>
+                                    <div class="label"> Password </div>
+                                    <div class="eror" id="error-4"></div>
+                                    <div class="champs"> <input type="password" name="password" id="password" error="error-4" placeholder="********"> </div>
+                                    <div class="label"> Confirmer Password </div>
+                                    <div class="eror" id="error-5"></div>
+                                    <div class="champs"> <input type="password" name="confirm" id="confirm" error="error-5" placeholder="********"> </div>
+                                    <div class="texteavatar">Avatar</div>
+                                    <div class="fichier">
+                                        <label for="file" class="label-file">Choisir un fichier</label> 
+                                        <input id="file" class="choisir" type="file" name="image" accept="image/*" onchange="loadFile(event)">
+                                    </div>
+                                    <button type="submit" class="creer" name="submit" id="connexion"> Créer compte </button>
+                                </form>
                             </div>
-                            <div class="creer"> <button type="submit" name="submit" id="connexion"> Créer compte </button> </div>
-                        </form>
+                            <div class="champavatar">
+                                <img id="output" class="avatarr">
+                                <div class="joueur">Avatar du joueur</div>
+                            </div>
+                        </div>
                     </div>
-                    <img id="output" class="avatarr"></div>
-                    <div class="joueur">Avatar du joueur</div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
 <script>
   var loadFile = function(event) {
     var output = document.getElementById('output');
