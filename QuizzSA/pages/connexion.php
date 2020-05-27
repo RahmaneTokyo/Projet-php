ab19cbb2d6f7df5702544f8d865d52ce12cd78e4
RahmaneTokyo <62186167+RahmaneTokyo@users.noreply.github.com>
2020-05-27 19:37:21 +0000

    Update connexion.php

--- QuizzSA/pages/connexion.php
+++ QuizzSA/pages/connexion.php
@@ -2,34 +2,22 @@
     <div class="col-xs-12 col-sm-12 col-md-6 zone-texte">
         <div class="container le_plaisir_de_jouer"> Le Plaisir de Jouer </div>
         <div class="container bienvenu"> Bienvenue sur la plateforme de quizz<br>SA. Veuillez vous connecter pour jouer </div>
-        <div class="container inscription"> Pas de compte ? <a href="#"> Inscrivez-vous </a> </div>
+        <div class="container inscription"> Pas de compte ? <a href="index.php?lien=inscription"> Inscrivez-vous </a> </div>
     </div>
-    <div class="col-xs-12 col-sm-12 col-md-6 d-flex align-items-center justify-content-center zone-connect">
-        
-            <form method="POST" class="d-flex align-items-center">
-                <fieldset>
-                    <div class="form-group">
-                        <div class="row">
-                            <div class="col-xs-6">
-                                <span class="iconify" data-icon="ant-design:user-outlined" data-inline="false" style="color: #A44545;"></span>
-                            </div>
-                            <div class="col-xs-6">
-                                <input type="text" class="form-control" id="login" name="login" placeholder="Login">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="row">
-                            <div class="col-xs-6">
-                                <span class="iconify" data-icon="uil:padlock" data-inline="false" style="color: #A44545;"></span>
-                            </div>
-                            <div class="col-xs-6">
-                                <input type="password" class="form-control" id="pwd" name="pwd" placeholder="Password">
-                            </div>
-                        </div>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </fieldset>
-            </form>
+    <div class="col-xs-12 col-sm-12 col-md-6 d-flex align-items-center zone-form">
+        <form class="form" id="form" method="post">
+            <div class="form-control">
+                <span class="iconify" data-icon="ant-design:user-outlined" data-inline="false" style="color: #A44545;">
+                </span>
+                <input type="text" name="login" id="login" placeholder="Login">
+                <small>Validation Error</small>
+            </div>
+            <div class="form-control">
+                <span class="iconify" data-icon="uil:padlock" data-inline="false" style="color: #A44545;"></span>
+                <input type="password" name="pwd" id="pwd" placeholder="Password">
+                <small>Validation Error</small>
+            </div>
+            <button>Connectez-vous</button>
+        </form>
     </div>
 </div>
